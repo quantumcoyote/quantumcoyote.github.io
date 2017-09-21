3ea5e28ff0dc5295ff5f7638e432dc076f71c9dc
Pere Miro <Pere.Miro@usd.edu>
2017-09-21 15:27:40 -0500

    remove USDCC add Jmol test

--- usdcc/build/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:53,filenames:["index.txt"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to USDCC\u2019s documentation!"],titleterms:{document:0,indic:0,tabl:0,usdcc:0,welcom:0}})
